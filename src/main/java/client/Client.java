fca315e5441ab5d0c85ae13457dd911c25505aea
Artem Shaputsko <artem.shaputko@gmail.com>
2025-02-15 02:52:18 +0300

    Fix bugs and continue download

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -10,10 +10,11 @@ import java.nio.channels.FileChannel;
 public class Client {
     private int port;
     private String serverIp;
-    private String lastServer;
-    private String lastFile;
+    long startTime;
     public static final int timeout = 30 * 1000;
-    public static final String PING_MESSAGE = "\1\2\3\4";
+    public static final String CLIENT_PING_MESSAGE = "ping";
+    public static final String SERVER_PING_MESSAGE = "pong";
+    private boolean isConnected = false;
 
     public Client() {
     }
@@ -24,7 +25,6 @@ public class Client {
     }
 
     public void connect(String ip, int port) {
-        lastServer = serverIp;
         this.serverIp = ip;
         this.port = port;
         connect();
@@ -34,6 +34,7 @@ public class Client {
         try (Socket socket = new Socket(serverIp, port)) {
             socket.setKeepAlive(true);
             socket.setSoTimeout(timeout);
+            isConnected = true;
             communicate(socket);
         } catch (SocketTimeoutException e) {
             System.out.println("Время ожидания вышло");
@@ -45,56 +46,65 @@ public class Client {
     private void communicate(Socket socket) throws IOException {
         OutputStream output = socket.getOutputStream();
         InputStream input = socket.getInputStream();
-        long startTime = System.currentTimeMillis();
+        startTime = System.currentTimeMillis();
         try (BufferedReader socketReader = new BufferedReader(new InputStreamReader(input));
-             BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
-             PrintWriter writer = new PrintWriter(output, true);) {
+             PrintWriter writer = new PrintWriter(output, true)) {
+            BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
             System.out.print("$ ");
-            while (true) {
+            while (isConnected) {
                 if (socketReader.ready()) {
-                    startTime = System.currentTimeMillis();
-                    System.out.println(socketReader.readLine());
+                    while (socketReader.ready()) {
+                        startTime = System.currentTimeMillis();
+                        System.out.println(socketReader.readLine());
+                    }
                     System.out.print("$ ");
                 }
-                if (consoleReader.ready()) {
+                while (consoleReader.ready()) {
                     String line = consoleReader.readLine();
                     writer.println(line);
                     String[] args = line.split(" ");
                     switch (args[0]) {
+                        case SERVER_PING_MESSAGE:
+                            writer.println(CLIENT_PING_MESSAGE);
                         case "close":
                             return;
                         case "download":
                             boolean cont = args.length > 3 && args[3].equalsIgnoreCase("continue");
                             if (args.length < 3) {
-                                System.out.println("Saving to " + args[1]);
+                                System.out.println("Сохранение в " + args[1]);
                                 downloadFile(input, output, args[1], cont);
                             } else {
                                 downloadFile(input, output, args[2], cont);
                             }
                     }
                 }
-                if (System.currentTimeMillis() - startTime > timeout) {
-                    writer.println(PING_MESSAGE);
-                    char[] pingMessage = new char[PING_MESSAGE.length() + 2];
-                    if (socketReader.read(pingMessage) == -1) {
-                        System.out.println("Соединение прервано");
-                        return;
-                    }
-                }
+                ping(writer, socketReader);
             }
         } catch (Exception e) {
             System.out.println("Превышено время ожидания, автоматическое отключение");
         }
     }
 
+    private void ping(PrintWriter writer, BufferedReader socketReader) throws IOException {
+        if (System.currentTimeMillis() - startTime > timeout) {
+            writer.println(CLIENT_PING_MESSAGE);
+            char[] pingMessage = new char[CLIENT_PING_MESSAGE.length() + 5];
+            if (socketReader.read(pingMessage) == -1) {
+                System.out.println("Соединение прервано");
+                isConnected = false;
+                return;
+            }
+            startTime = System.currentTimeMillis();
+        }
+    }
+
     private void downloadFile(InputStream is, OutputStream os, String fileName, boolean cont) throws IOException {
-        lastFile = fileName;
         File file = new File(fileName);
 
         byte[] fileSizeBytes = new byte[Integer.BYTES];
         long start = System.nanoTime();
         if (is.read(fileSizeBytes) < Integer.BYTES) {
-            System.out.println("Can`t determine file size");
+            System.out.println("Не получилось определить размер файла");
         }
         int fileSize = ByteBuffer.wrap(fileSizeBytes).order(ByteOrder.LITTLE_ENDIAN).getInt();
         if (fileSize == 0) {
@@ -102,7 +112,7 @@ public class Client {
             return;
         }
 
-        try (FileOutputStream fos = new FileOutputStream(file);
+        try (FileOutputStream fos = new FileOutputStream(file, cont);
              FileChannel channel = fos.getChannel()) {
             os.write(1);
 
--- src/main/java/client/Main.java
+++ src/main/java/client/Main.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String line = "";
+        String line;
         Scanner scanner = new Scanner(System.in);
         Client client = new Client();
         while (true) {
-            System.out.print("Inter server address pr exit:\n# ");
+            System.out.print("Введите адрес сервера или `exit`:\n# ");
             line = scanner.nextLine();
             if (line.equalsIgnoreCase("exit")) {
                 return;
